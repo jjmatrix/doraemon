7b05c6dc6c79b3c767b74c86f8f5246cc31eec09
jjmatrix <coral@coraldeMacBook-Pro.local>
2016-02-05 23:37:34 +0800

    init

---
+++ doraemon-core/src/main/java/org/jmatrx/utils/JsonSerializeUtils.java
@@ -0,0 +1,18 @@
+package org.jmatrx.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author jmatrix
+ * @date 16/2/5
+ */
+public class JsonSerializeUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JsonSerializeUtils.class);
+
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+
+
+}
